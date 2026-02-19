446d4f028910c897a8ca5342b4136eb14482800d
priyadharshinis2003 <priyasivam2003@gmail.com>
2026-02-19 15:57:11 +0530

    the code is print

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-console.log("welcome to the git course");
+console.log("welcome to the git playlist");
+for(var i = 0; i < 5; i++) {
+    console.log("priya: " + i);
+}
 
 //[main (root-commit) 09b6b7c] readme
